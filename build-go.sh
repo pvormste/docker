df4a4cbff714a185f30eef1f3becc9c256904712
Patric Vormstein <pvormstein@googlemail.com>
2019-07-03 20:44:31 +0200

    go 1.12.5

--- build-go.sh
+++ build-go.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 # Versions
-GO_VERSION=1.12.4
+GO_VERSION=1.12.5
 ALPINE_VERSION=3.9
 DOCKER_VERSION=18.09
 
 # DIRECTORIES
 DIRECTORY=${PWD}
 OFFICIAL_IMAGES_DIRECTORY=${DIRECTORY}/official-images
+
+# Check for docker command
+if ! hash docker 2>/dev/null; then
+  echo "ERROR: docker cli is not installed. aborting."
+  exit 1
+fi
 
 # Build Image
 IMAGE_NAME=pvormste/docker-go
@@ -16,6 +22,12 @@ GO_MINOR_VERSION=${GO_VERSION%.*}
 cd ${DOCKER_VERSION}
 docker build -t ${IMAGE_NAME}:${GO_VERSION} --build-arg GO_VERSION=${GO_VERSION} --build-arg ALPINE_VERSION=${ALPINE_VERSION} .
 docker tag ${IMAGE_NAME}:${GO_VERSION} ${IMAGE_NAME}:${GO_MINOR_VERSION}
+
+# Check if docker commands were successful
+if [ ! $? = 0 ]; then
+  echo "ERROR: docker command execution was not successful. aborting."
+  exit 2
+fi
 
 # Test image
 if [ ! -d "${OFFICIAL_IMAGES_DIRECTORY}" ]; then
