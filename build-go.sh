7649bfffff9c4515d4b79413ea2ae8b793906073
Patric Vormstein <pvormstein@googlemail.com>
2020-01-11 11:18:44 +0100

    wip. try it put

--- build-go.sh
+++ build-go.sh
@@ -9,6 +9,21 @@ DOCKER_VERSION=19.03
 DIRECTORY=${PWD}
 OFFICIAL_IMAGES_DIRECTORY=${DIRECTORY}/official-images
 
+# IMAGE
+IMAGE_NAME=pvormste/docker-go
+GO_MINOR_VERSION=${GO_VERSION%.*}
+
+# Only show image names when desired
+if [ $1 = 'show-latest' ]; then
+  echo "${IMAGE_NAME}:${GO_VERSION}"
+  exit 0;
+fi
+
+if [ $1 = 'show-minor' ]; then
+  echo "${IMAGE_NAME}:${GO_MINOR_VERSION}"
+  exit 0;
+fi
+
 # Check for docker command
 if ! hash docker 2>/dev/null; then
   echo "ERROR: docker cli is not installed. aborting."
@@ -16,9 +31,6 @@ if ! hash docker 2>/dev/null; then
 fi
 
 # Build Image
-IMAGE_NAME=pvormste/docker-go
-GO_MINOR_VERSION=${GO_VERSION%.*}
-
 cd ${DOCKER_VERSION}
 docker build -t ${IMAGE_NAME}:${GO_VERSION} --build-arg GO_VERSION=${GO_VERSION} --build-arg ALPINE_VERSION=${ALPINE_VERSION} .
 docker tag ${IMAGE_NAME}:${GO_VERSION} ${IMAGE_NAME}:${GO_MINOR_VERSION}
